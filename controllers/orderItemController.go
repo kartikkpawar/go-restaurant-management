c97e44a9a3b21550e000f085a606acdd14fa6dab
Kartik Pawar <knpawar789@gmail.com>
2025-04-03 22:00:29 +0530

    controllers outline

---
+++ controllers/orderItemController.go
@@ -0,0 +1,21 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func GetOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func CreateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+func GetOrderItemsbyOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
